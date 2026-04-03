ac359d36ec9203fe42e5a7a93541ab84f2c26b8a
rohan1205 <rohanydv1305@email.com>
2026-04-03 11:56:57 +0530

    eslind build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
